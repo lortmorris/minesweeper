86737019886ee9f671fb4510053c706ed9d7d14c
cesar casas <cesarcasas@bsdsolutions.com.ar>
2019-10-16 14:56:30 -0300

    build

---
+++ build/precache-manifest.ae99f648c8fb10d3dcf3351097a9ef13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9079fccc2d1e3a772c7c50e0a29c2a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0c4ce0a76fc9afb2f5",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5e2f0d9dd2464c8ae000",
+    "url": "/static/css/main.3f4681a5.chunk.css"
+  },
+  {
+    "revision": "0b0c4ce0a76fc9afb2f5",
+    "url": "/static/js/2.3473c19d.chunk.js"
+  },
+  {
+    "revision": "5e2f0d9dd2464c8ae000",
+    "url": "/static/js/main.fe54d4ad.chunk.js"
+  },
+  {
+    "revision": "cd7dd90b89f1ae64944d",
+    "url": "/static/js/runtime-main.a3f01345.js"
+  }
+]);
